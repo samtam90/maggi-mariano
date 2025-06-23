a457cf89b309423959fc748d332a88ff1b0ea3a5
samtam90 <samuele.tamburi@gmail.com>
2025-06-23 18:16:31 +0200

    a

--- src/templates/MainContent/index.jsx
+++ src/templates/MainContent/index.jsx
@@ -1,7 +1,17 @@
 "use client";
 
 import React, { memo } from "react";
-import { LocationsGridData, renderLocationsGrid } from "../auxiliary";
+import {
+  renderMaps,
+  renderPageFooter,
+  renderServicesNavGrid,
+  renderTitleSection,
+  renderTopNavBar,
+  LocationsGridData,
+  renderLocationsGrid,
+} from "../auxiliary";
+import appConfig from "../../../app.config";
+import Header from "../../components/Header";
 import ContactSection, {
   PhoneNumber,
   Image,
@@ -13,7 +23,6 @@ import ContentArticle, {
 } from "../../components/ContentArticle";
 import tailwindConfig from "../../../tailwind.config";
 import { ContactFormData } from "../../components/ContactForm";
-import AuxTemplate from "../auxiliary/AuxTemplate";
 
 /**
  * @typedef {{
@@ -55,53 +64,38 @@ function MainContentTemplate({ mobile, sections, onContactFormSubmit }) {
   } = sections;
 
   return (
-    <AuxTemplate mobile={mobile}>
-      <ContentArticle
-        title={{
-          ...mainContentSectionData.title,
-          fillColor: tailwindConfig.theme.extend.colors.red.primary,
-        }}
-        titleHeadingElements={{ article: "h1", section: "h2" }}
-        classNames={{
-          root: "py-4 lg:py-12 px-4 lg:px-0 max-w-6xl mx-auto",
-          section: {
-            paragraph: "mb-4",
-          },
-        }}
-      >
-        {mainContentSectionData.sections?.map(
-          (
-            { title, paragraphs, mediaElements, mediaPosition, classNames },
-            index
-          ) => (
-            <ContentArticle.Section
-              key={title ? `${title}-${index}` : index}
-              title={title}
-              paragraphs={paragraphs}
-              mediaElements={mediaElements}
-              mediaPosition={mediaPosition || "right"}
-              classNames={classNames}
-            />
-          )
-        )}
-      </ContentArticle>
-      {contactsSectionData && (
-        <ContactSection
-          {...contactsSectionData}
-          ids={testIDs.contactForm}
-          variant={mobile ? "vertical" : "horizontal"}
-          classNames={{
-            contentWrapper: "max-w-6xl",
-          }}
-          onFormSubmit={onContactFormSubmit}
-        />
-      )}
-      {locationsSectionData &&
-        renderLocationsGrid({
-          ...locationsSectionData,
-          classNames: { root: "py-8 lg:py-24 px-4 lg:px-0" },
+    <main>
+      {renderTopNavBar({
+        items: appConfig.data.topNavBar.items,
+        maxWidth: "6xl",
+        className: "hidden md:block",
+      })}
+      <div className="sticky top-0 z-top bg-white shadow-md">
+        <Header mobile={mobile} classNames={{ contentWrapper: "max-w-6xl" }} />
+      </div>
+      
+      {renderTitleSection(appConfig.data.mainTitle)}
+      <section key="services">
+        {renderServicesNavGrid({
+          ...appConfig.data.servicesGrid,
+          className: "max-w-6xl",
         })}
-    </AuxTemplate>
+      </section>
+      {renderMaps({
+        data: appConfig.data.maps,
+        dimensions: { width: "100%", height: "300" },
+        classNames: {
+          root: "px-4 lg:max-w-6xl lg:px-0 mx-auto mt-4 lg:mt-0",
+          map: "pb-8 lg:pb-0",
+        },
+      })}
+      {renderPageFooter({
+        logo: appConfig.data.footer.logo,
+        sections: appConfig.data.footer.sections,
+        contentMaxWidth: "6xl",
+        variant: mobile ? "vertical" : "horizontal",
+      })}
+    </main>
   );
 }
 
