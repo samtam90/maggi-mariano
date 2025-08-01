d000c7f9aa897d9d3cc56f55b29ee289f1d6d734
samtam90 <samuele.tamburi@gmail.com>
2025-08-01 17:58:46 +0200

    -.-

---
+++ src/app/autospurgo/autospurgo-foiano-della-chiana/page.js
@@ -0,0 +1,7 @@
+import { getMetadata } from "../../servizi/frantoio-mobile-inerti/page";
+import { withBaseProps } from "../page";
+
+export const metadata = getMetadata({
+  title: "Autospurgo Foiano della Chiana",
+});
+export default withBaseProps({ title: "Autospurgo Foiano della Chiana" });
---
+++ src/app/servizi/spurgo-fosse-biologiche/spurgo-fosse-biologiche-foiano-della-chiana/page.js
@@ -0,0 +1,7 @@
+import { getMetadata } from "../../frantoio-mobile-inerti/page";
+import { withBaseProps } from "../page";
+
+export const metadata = getMetadata({
+  title: "Spurgo fosse biologiche Foiano della Chiana",
+});
+export default withBaseProps({ title: "Spurgo fosse biologiche Foiano della Chiana" });
